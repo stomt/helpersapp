680bec58ff81a82701ef3f405758a07f9853c2cd
Philipp Zentner <philipp.zentner@stomt.com>
2015-04-28 17:28:45 +0200

    Styles

--- laravel/app/views/hello.blade.php
+++ laravel/app/views/hello.blade.php
@@ -9,6 +9,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
+    <link rel="stylesheet" href="css/main.css">
 
     <!-- Scripts -->
     <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>-->
